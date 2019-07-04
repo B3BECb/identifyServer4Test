11b91c9b8ca18bd5d67372e1c3b58ba522497b3f
danith <danith@mallenom.ru>
2019-07-04 12:42:52 +0300

    --

--- IdentityVueTest/ClientApp/src/views/Login.vue
+++ IdentityVueTest/ClientApp/src/views/Login.vue
@@ -1,31 +1,47 @@
 <template>
-	<div class = "view login">
-		<div></div>
-		<div></div>
-		<form @submit.prevent = "someAction()">
-			<md-card class = "md-layout-item md-size-50 md-small-size-100">
-				<md-card-header>
-					<div class = "md-title">Users</div>
-				</md-card-header>
+	<div>
+		<div class = "md-layout md-alignment-center">
+			<div></div>
+			<div></div>
+			<md-card class = "md-layout-item md-size-100 md-small-size-50">
+				<form @submit.prevent = "someAction()">
+					<md-card-header>
+						<div class = "md-title">Mallenom systems</div>
+					</md-card-header>
 
-				<md-card-content>
+					<md-card-content>
+						<md-field :class = "getValidationClass('login')">
+							<label>{{$t('login')}}</label>
+							<md-input v-model = "login"></md-input>
+							<span class = "md-error"
+								  v-if = "!$v.login.required">{{$t('loginError')}}</span>
+						</md-field>
+					</md-card-content>
 
-				</md-card-content>
+					<md-card-content>
+						<md-field :class = "getValidationClass('password')">
+							<label>{{$t('password')}}</label>
+							<md-input v-model = "password"></md-input>
+							<span class = "md-error"
+								  v-if = "!$v.password.required">{{$t('passwordError')}}</span>
+						</md-field>
+					</md-card-content>
 
-				<md-progress-bar md-mode = "indeterminate"
-								 v-if = "sending" />
+					<md-progress-bar md-mode = "indeterminate"
+									 v-if = "sending" />
 
-				<md-card-actions>
-					<md-button type = "submit"
-							   class = "md-primary"
-							   :disabled = "$v.$invalid">Create user
-					</md-button>
-				</md-card-actions>
+					<md-card-actions>
+						<md-button type = "submit"
+								   class = "md-primary"
+								   :disabled = "sending">Create user
+						</md-button>
+					</md-card-actions>
+				</form>
 			</md-card>
-		</form>
-		<div></div>
-		<div></div>
-		<div>langs</div>
+			<div></div>
+			<div></div>
+		</div>
+		<div class="md-bottom-bar">langs</div>
 	</div>
 </template>
 
@@ -33,38 +49,49 @@
 	import { validationMixin } from "vuelidate";
 	import {
 		required,
-		email,
-		minLength,
-		maxLength,
 	} from "vuelidate/lib/validators";
 	import { Component, Vue } from "vue-property-decorator";
-	import { MdButton, MdContent, MdCard, MdProgress } from 'vue-material/dist/components';
+	// @ts-ignore
+	import { MdButton, MdContent, MdCard, MdProgress, MdField } from 'vue-material/dist/components';
 
 	Vue.use(MdButton);
 	Vue.use(MdContent);
 	Vue.use(MdCard);
 	Vue.use(MdProgress);
+	Vue.use(MdField);
 
 	@Component({
 		mixins:      [validationMixin],
 		validations: {
-			passportData: {
+			login:    {
 				required,
-				validFormat: (val: string) => /^\d{4} \d{6}$/.test(val),
 			},
-			name:         {
-				required,
-				maxLength: maxLength(10),
-				alpha:     (val: string) => /^[а-яё]*$/i.test(val),
-			},
+			password: {},
 		},
 	})
 	export default class Login
 		extends Vue
 	{
-		passportData = null;
-		name         = null;
-		sending      = true;
+		login    = null;
+		password = null;
+		sending  = false;
+
+		getValidationClass(fieldName)
+		{
+			let field = this.$v[fieldName];
+
+			if(field)
+			{
+				return {
+					'md-invalid': field.$invalid && field.$dirty,
+				};
+			}
+		}
+
+		someAction()
+		{
+			this.$v.$touch();
+		}
 	}
 </script>
 
