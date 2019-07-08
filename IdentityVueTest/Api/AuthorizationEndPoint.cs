8d26f59fb96114ddf3d45c460c3bd43806da04c5
danith <danith@mallenom.ru>
2019-07-08 15:15:31 +0300

    --

--- IdentityVueTest/Api/AuthorizationEndPoint.cs
+++ IdentityVueTest/Api/AuthorizationEndPoint.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Events;
 using IdentityServer4.Services;
 using IdentityServer4.Stores;
 using IdentityVueTest.Models;
@@ -19,6 +20,10 @@ namespace IdentityVueTest.Api
 		public string Login { get; set; }
 
 		public string Password { get; set; }
+
+		public string ReturnUrl { get; set; }
+
+		public bool RememberLogin { get; set; }
 	}
 
 	[SecurityHeaders]
@@ -56,9 +61,52 @@ namespace IdentityVueTest.Api
 		// POST api/<controller>
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Post([FromForm]AuthorizationData data)
+		public async Task<IActionResult> Post([FromForm]AuthorizationData model)
 		{
 			var context = await Interaction.GetAuthorizationContextAsync(model.ReturnUrl);
+
+			if (ModelState.IsValid)
+			{
+				var result = await SignInManager.PasswordSignInAsync(model.Login, model.Password, model.RememberLogin, lockoutOnFailure: true);
+				if (result.Succeeded)
+				{
+					var user = await UserManager.FindByNameAsync(model.Login);
+					await Events.RaiseAsync(new UserLoginSuccessEvent(user.UserName, user.Id, user.UserName));
+
+					if (context != null)
+					{
+						if (await ClientStore.IsPkceClientAsync(context.ClientId))
+						{
+							// if the client is PKCE then we assume it's native, so this change in how to
+							// return the response is for better UX for the end user.
+							return View("Redirect", new RedirectViewModel { RedirectUrl = model.ReturnUrl });
+						}
+
+						// we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
+						return Redirect(model.ReturnUrl);
+					}
+
+					// request for a local page
+					if (Url.IsLocalUrl(model.ReturnUrl))
+					{
+						return Redirect(model.ReturnUrl);
+					}
+					else if (string.IsNullOrEmpty(model.ReturnUrl))
+					{
+						return Redirect("~/");
+					}
+					else
+					{
+						// user might have clicked on a malicious link - should be logged
+						throw new Exception("invalid return URL");
+					}
+				}
+
+				await Events.RaiseAsync(new UserLoginFailureEvent(model.Login, "invalid credentials"));
+				ModelState.AddModelError(string.Empty, AccountOptions.InvalidCredentialsErrorMessage);
+			}
+
+			return new NotFoundResult();
 		}
 	}
 }
---
+++ IdentityVueTest/Api/RedirectViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityVueTest.Api
+{
+	public class RedirectViewModel
+	{
+		public string RedirectUrl { get; set; }
+	}
+}
