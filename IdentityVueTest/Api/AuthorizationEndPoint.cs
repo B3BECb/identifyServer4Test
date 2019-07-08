267481a2448857ef284e549c021a1d07f10f42a0
danith <danith@mallenom.ru>
2019-07-08 13:06:01 +0300

    --

--- IdentityVueTest/Api/AuthorizationEndPoint.cs
+++ IdentityVueTest/Api/AuthorizationEndPoint.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using IdentityVueTest.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,11 +26,39 @@ namespace IdentityVueTest.Api
 	[Route("api/v1/login")]
 	public class AuthorizationEndPoint : Controller
 	{
+		private UserManager<User> UserManager { get; set; }
+
+		private SignInManager<User> SignInManager { get; set; }
+
+		private IIdentityServerInteractionService Interaction { get; set; }
+
+		private IClientStore ClientStore { get; set; }
+
+		private IAuthenticationSchemeProvider SchemeProvider { get; set; }
+
+		private IEventService Events { get; set; }
+
+		public AuthorizationEndPoint(UserManager<User> userManager,
+			SignInManager<User> signInManager,
+			IIdentityServerInteractionService interaction,
+			IClientStore clientStore,
+			IAuthenticationSchemeProvider schemeProvider,
+			IEventService events)
+		{
+			UserManager = userManager;
+			SignInManager = signInManager;
+			Interaction = interaction;
+			ClientStore = clientStore;
+			SchemeProvider = schemeProvider;
+			Events = events;
+		}
+
 		// POST api/<controller>
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public void Post([FromForm]AuthorizationData data)
+		public async Task<IActionResult> Post([FromForm]AuthorizationData data)
 		{
+			var context = await Interaction.GetAuthorizationContextAsync(model.ReturnUrl);
 		}
 	}
 }
