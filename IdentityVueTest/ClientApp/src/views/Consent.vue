300ad8afd9138d2443094bf029267fefa89dc5f5
danith <danith@mallenom.ru>
2019-07-09 17:55:45 +0300

    --

--- IdentityVueTest/ClientApp/src/views/Consent.vue
+++ IdentityVueTest/ClientApp/src/views/Consent.vue
@@ -1,14 +1,25 @@
 <template>
-
+	<div>{{this.Test}}</div>
 </template>
 
 <script lang = "ts">
 	import { Component, Vue } from "vue-property-decorator";
+	import Axios from "axios";
 
 	@Component
 	export default class Consent
 		extends Vue
 	{
+		public Test:  string = "";
+
+		async beforeMount()
+		{
+			const data = await Axios.create({
+				url: "/consent"
+			});
+
+			console.log(data);
+		}
 	};
 </script>
 
