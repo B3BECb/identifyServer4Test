d3e7fc7fadfb394f48d643de9f11d4db1535d1ce
danith <danith@mallenom.ru>
2019-07-17 17:33:33 +0300

    --

--- IdentityVueTest/ClientApp/src/views/Consent.vue
+++ IdentityVueTest/ClientApp/src/views/Consent.vue
@@ -27,28 +27,28 @@
 						 v-if = "Model.IdentityScopes.length">
 
 						<md-list>
-							<md-subheader class="md-layout md-alignment-center-left">
+							<md-subheader class = "md-layout md-alignment-center-left">
 								<!--<span class="md-layout-item">-->
-									<!--<md-icon class = "icon">account_circle</md-icon>-->
+								<!--<md-icon class = "icon">account_circle</md-icon>-->
 								<!--</span>-->
-								<span class="md-layout-item">Personal Information</span>
+								<span class = "md-layout-item">Personal Information</span>
 							</md-subheader>
 							<md-list-item v-for = "scope in Model.IdentityScopes">
 								<md-checkbox name = "ScopesConsented"
 											 v-bind:id = "'scopes_' + scope.Name"
 											 v-bind:value = "scope.Name"
-											 v-bind:checked = "scope.Checked"
+											 v-model = "scope.Checked"
 											 v-bind:disabled = "scope.Required" />
 								<span class = "md-list-item-text">
 									<span>
 										<span>
 											<span>{{scope.DisplayName}}</span>
 											<span v-if = "scope.Required"
-												  class="md-caption"><em> (required)</em></span>
+												  class = "md-caption"><em> (required)</em></span>
 										</span>
 									</span>
 									<span v-if = "scope.Description"
-										 class = "md-caption">
+										  class = "md-caption">
 										<label v-bind:for = "'scopes' + scope.Name">{{scope.Description}}</label>
 									</span>
 								</span>
@@ -59,26 +59,26 @@
 									   v-bind:value = "scope.Name" />
 							</md-list-item>
 							<md-divider v-if = "Model.ResourceScopes.length"></md-divider>
-							<md-subheader class="md-layout md-alignment-center-left"
+							<md-subheader class = "md-layout md-alignment-center-left"
 										  v-if = "Model.ResourceScopes.length">
 								<!--<span class="md-layout-item">-->
 								<!--<md-icon class = "icon">account_circle</md-icon>-->
 								<!--</span>-->
-								<span class="md-layout-item">Application Access</span>
+								<span class = "md-layout-item">Application Access</span>
 							</md-subheader>
 							<md-list-item v-if = "Model.ResourceScopes.length"
 										  v-for = "scope in Model.ResourceScopes">
 								<md-checkbox name = "ScopesConsented"
-										v-bind:id = "'scopes_' + scope.Name"
-										v-bind:value = "scope.Name"
-										v-bind:checked = "scope.Checked"
-										v-bind:disabled = "scope.Required" />
+											 v-bind:id = "'scopes_' + scope.Name"
+											 v-bind:value = "scope.Name"
+											 v-model = "scope.Checked"
+											 v-bind:disabled = "scope.Required" />
 								<span class = "md-list-item-text">
 									<span>
 										<span>
 											<span>{{scope.DisplayName}}</span>
 											<span v-if = "scope.Required"
-												  class="md-caption"><em> (required)</em></span>
+												  class = "md-caption"><em> (required)</em></span>
 										</span>
 									</span>
 									<span v-if = "scope.Description"
@@ -111,136 +111,6 @@
 				</md-button>
 			</md-card-actions>
 		</md-card>
-		<!--<md-content v-if = "Model"
-					class = "md-layout-item md-elevation-3 md-size-50">
-			<div class = "title">
-				<div v-if = "Model.ClientLogoUrl"
-					 class = "logo"><img v-bind:src = "Model.ClientLogoUrl"></div>
-				<div class = "appInfo">
-					<div class = "name">{{Model.ClientName}}</div>
-					<div>is requesting your permission</div>
-				</div>
-			</div>
-
-			<div class = "row">
-				<div class = "col-sm-8">
-
-					<form @submit = "submit"
-						  action = "/api/v1/consent"
-						  method = "post">
-						<input type = "hidden"
-							   v-model = "XSRF"
-							   name = "XSRF-TOKEN-FIELD" />
-						<input type = "hidden"
-							   v-model = "ReturnUrl"
-							   name = "returnUrl" />
-
-						<div>Uncheck the permissions you do not wish to grant.</div>
-
-						<div v-if = "Model.IdentityScopes.length"
-							 class = "panel panel-default consent-buttons">
-							<div class = "panel-heading">
-								<span class = "glyphicon glyphicon-user"></span>
-								Personal Information
-							</div>
-							<ul class = "list-group">
-								<li v-for = "scope in Model.IdentityScopes"
-									class = "list-group-item">
-									<label>
-										<input class = "consent-scopecheck"
-											   type = "checkbox"
-											   name = "ScopesConsented"
-											   v-bind:id = "'scopes_' + scope.Name"
-											   v-bind:value = "scope.Name"
-											   v-bind:checked = "scope.Checked"
-											   v-bind:disabled = "scope.Required" />
-										<input v-if = "scope.Required"
-											   type = "hidden"
-											   name = "ScopesConsented"
-											   v-bind:value = "scope.Name" />
-										<strong>{{scope.DisplayName}}</strong>
-										<span v-if = "scope.Emphasize"
-											  class = "glyphicon glyphicon-exclamation-sign"></span>
-									</label>
-									<span v-if = "scope.Required"><em>(required)</em></span>
-									<div v-if = "scope.Description"
-										 class = "consent-description">
-										<label v-bind:for = "'scopes' + scope.Name">{{scope.Description}}</label>
-									</div>
-								</li>
-							</ul>
-						</div>
-
-						<div v-if = "Model.ResourceScopes.length"
-							 class = "panel panel-default">
-							<div class = "panel-heading">
-								<span class = "glyphicon glyphicon-tasks"></span>
-								Application Access
-							</div>
-							<ul class = "list-group">
-								<li v-for = "scope in Model.ResourceScopes"
-									class = "list-group-item">
-									<label>
-										<input class = "consent-scopecheck"
-											   type = "checkbox"
-											   name = "ScopesConsented"
-											   v-bind:id = "'scopes_' + scope.Name"
-											   v-bind:value = "scope.Name"
-											   v-bind:checked = "scope.Checked"
-											   v-bind:disabled = "scope.Required" />
-										<input v-if = "scope.Required"
-											   type = "hidden"
-											   name = "ScopesConsented"
-											   v-bind:value = "scope.Name" />
-										<strong>{{scope.DisplayName}}</strong>
-										<span v-if = "scope.Emphasize"
-											  class = "glyphicon glyphicon-exclamation-sign"></span>
-									</label>
-									<span v-if = "scope.Required"><em>(required)</em></span>
-									<div v-if = "scope.Description"
-										 class = "consent-description">
-										<label v-bind:for = "'scopes' + scope.Name">{{scope.Description}}</label>
-									</div>
-								</li>
-							</ul>
-						</div>
-
-						<div v-if = "Model.AllowRememberConsent"
-							 class = "consent-remember">
-							<label>
-								<input class = "consent-scopecheck"
-									   name = "RememberConsent" />
-								<strong>Remember My Decision</strong>
-							</label>
-						</div>
-
-						<div class = "consent-buttons">
-							<button name = "button"
-									value = "yes"
-									class = "btn btn-primary"
-									autofocus>Yes, Allow
-							</button>
-							<button name = "button"
-									value = "no"
-									class = "btn">No, Do Not Allow
-							</button>
-							<a v-if = "Model.ClientUrl"
-							   class = "pull-right btn btn-default"
-							   target = "_blank"
-							   @href = "Model.ClientUrl">
-								<span class = "glyphicon glyphicon-info-sign"></span>
-								<strong>{{Model.ClientName}}</strong>
-							</a>
-						</div>
-					</form>
-				</div>
-			</div>
-
-		</md-content>
-		<md-content v-else
-					class = "md-layout-item">
-			Something went wrong
-		</md-content>-->
 	</form>
 </template>
 
@@ -333,8 +203,6 @@
 
 			data = this.CapitalizeKeys(data.data);
 
-			console.log(data);
-
 			this.Model = data;
 
 			console.log(this.Model);
@@ -345,6 +213,10 @@
 			this.ReturnUrl = (this.$route.query as any).ReturnUrl || "";
 
 			this.XSRF = this.$cookies.get("XSRF-TOKEN");
+
+			console.log(this.Model);
+
+			e.preventDefault();
 
 			return true;
 		}
@@ -385,19 +257,15 @@
 	@import '~material-icons-font/sass/sizing';
 	@import '~material-icons-font/sass/coloring';
 
-	.icon
-	{
-		&.emphasize
-		{
+	.icon {
+		&.emphasize {
 			width: 24px !important;
 			margin-left: 8px;
 		}
 	}
 
-	.consent
-	{
-		&.displayName
-		{
+	.consent {
+		&.displayName {
 			display: flex;
 
 		}
