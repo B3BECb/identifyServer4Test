a6ab1e6536de2e83407ba6f107289c6d8bff7159
danith <danith@mallenom.ru>
2019-07-24 18:08:45 +0300

    --

--- IdentityVueTest/ClientApp/src/views/Index.vue
+++ IdentityVueTest/ClientApp/src/views/Index.vue
@@ -1,12 +1,11 @@
 <template>
-	<div>
-		<div>
-			<div>Welcome to Mallenom Systems OIDC server</div>
-			<md-list>
-				<md-list-item :to="{name: 'grants'}">Stored grants</md-list-item>
-				<md-list-item :to="{name: 'logout'}">Logout</md-list-item>
-			</md-list>
-		</div>
+	<div class="content">
+		<div class="title md-display-2">Welcome to Mallenom Systems OIDC server</div>
+		<md-list>
+			<md-subheader>Actions</md-subheader>
+			<md-list-item :to="{name: 'grants'}">Stored grants</md-list-item>
+			<md-list-item :to="{name: 'logout'}">Logout</md-list-item>
+		</md-list>
 	</div>
 </template>
 
@@ -14,10 +13,12 @@
 	import { Component, Vue } from "vue-property-decorator";
 	import {
 		MdList,
+		MdSubheader,
 		// @ts-ignore
 	} from "vue-material/dist/components";
 
 	Vue.use(MdList);
+	Vue.use(MdSubheader);
 
 	@Component
 	export default class Index
@@ -27,6 +28,34 @@
 	};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+	.content
+	{
+		display: grid;
 
+		grid-template-columns: 1fr 2fr 1fr 1fr 1fr 2fr 1fr;
+		grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+
+		width: 100vw;
+		height: 100vh;
+	}
+
+	.title
+	{
+		grid-column-start: 2;
+		grid-column-end: 6;
+
+		grid-row-start: 2;
+	}
+
+	.md-list {
+		width: 320px;
+		max-width: 100%;
+		display: inline-block;
+		vertical-align: top;
+		border: 1px solid rgba(#000, .12);
+
+		grid-column-start: 2;
+		grid-row-start: 3;
+	}
 </style>
